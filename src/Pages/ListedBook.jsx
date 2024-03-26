d970008579fba4005860578c4326b4ca7767d2fc
sujonifty <sujonifty@gmail.com>
2024-03-27 00:09:44 +0600

    completed listed books section

--- src/Pages/ListedBook.jsx
+++ src/Pages/ListedBook.jsx
@@ -1,16 +1,41 @@
 import UseLocalStorage from "../components/Hooks/UseLocalStorage";
 import ListCard from "../components/ListCard";
+import { IoIosArrowDropdown } from "react-icons/io";
 
 
 const ListedBook = () => {
-    const [localStorageData ] = UseLocalStorage();
+    const [localStorageData] = UseLocalStorage();
     console.log('aa', localStorageData);
     return (
         <div>
-            {
-                localStorageData.map((item, index)=><ListCard key={index} item={item}></ListCard>)
-            }
-            
+
+            <div>
+                <div className="text-3xl font-bold  p-5 rounded-xl my-4 text-center bg-gray-200">
+                    <h1>Books</h1>
+                </div>
+                <select className="select  text-white bg-[#23BE0A] w-40" >
+                    <option disabled selected className="bg-white text-black font-semibold">Sort by</option>
+                    <option className="bg-white text-black font-semibold" >Rating</option>
+                    <option className="bg-white text-black font-semibold" >Number of pages</option>
+                    <option className="bg-white text-black font-semibold" >Published year</option>
+                </select>
+
+                <div role="tablist" className="tabs tabs-lifted">
+                    <input type="radio" name="my_tabs_2" role="tab" className="tab " aria-label="Read Books" />
+                    <div role="tabpanel" className="tab-content bg-base-100 border-base-300 space-y-10 rounded-box p-6">
+                        {
+                            localStorageData.map((item, index) => <ListCard key={index} item={item}></ListCard>)
+                        }
+                    </div>
+
+                    <input type="radio" name="my_tabs_2" role="tab" className="tab" aria-label="Wishlist Books" checked />
+                    <div role="tabpanel" className="tab-content bg-base-100 border-base-300 rounded-box p-6">Tab content 2</div>
+
+
+                </div>
+
+            </div>
+
         </div>
     );
 };
--- src/components/ListCard.jsx
+++ src/components/ListCard.jsx
@@ -1,20 +1,29 @@
+import PropTypes from 'prop-types';
 
-const ListCard = ({item}) => {
-    console.log(item)
+const ListCard = ({ item }) => {
+    const { image, tags, review, bookName, author, category, rating, totalPages, publisher, yearOfPublishing } = item;
     return (
         <div>
-            {/* <div role="tablist" className="tabs tabs-lifted">
-                <input type="radio" name="my_tabs_2" role="tab" className="tab" aria-label="Tab 1" />
-                <div role="tabpanel" className="tab-content bg-base-100 border-base-300 rounded-box p-6">Tab content 1</div>
+            
 
-                <input type="radio" name="my_tabs_2" role="tab" className="tab" aria-label="Tab 2" checked />
-                <div role="tabpanel" className="tab-content bg-base-100 border-base-300 rounded-box p-6">Tab content 2</div>
+            <div className="hero min-h-screen  bg-red-200">
+                <div className="hero-content flex-col lg:flex-row-reverse">
+                    <div className="text-center lg:text-left">
+                        <h1 className="text-2xl font-bold">{bookName}</h1>
+                        <p className="py-6">Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda excepturi exercitationem quasi. In deleniti eaque aut repudiandae et a id nisi.</p>
+                    </div>
+                    <div className="card shrink-0  max-w-sm shadow-2xl bg-base-100">
+                        <div className="card-body">
+                        <img src={image} className=" rounded-lg " />
 
-                <input type="radio" name="my_tabs_2" role="tab" className="tab" aria-label="Tab 3" />
-                <div role="tabpanel" className="tab-content bg-base-100 border-base-300 rounded-box p-6">Tab content 3</div>
-            </div> */}
+                        </div>
+                    </div>
+                </div>
+            </div>
         </div>
     );
 };
-
+ListCard.propTypes = {
+    item: PropTypes.object
+};
 export default ListCard;
