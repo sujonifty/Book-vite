e4303ee15b21359be949ad61dc44e552d4c2eb80
sujonifty <sujonifty@gmail.com>
2024-03-26 07:09:57 +0600

    completed all books section

--- src/components/Banner.jsx
+++ src/components/Banner.jsx
@@ -6,10 +6,10 @@ const Banner = () => {
             <div className="hero min-h-screen bg-base-200">
                 <div className="hero-content flex-col lg:flex-row-reverse">
                     <img src="banner.png" className="max-w-sm rounded-lg shadow-2xl" />
-                    <div>
-                        <h1 className="text-5xl font-bold">Books to freshen up your bookshelf</h1>
+                    <div className=" flex flex-col justify-start">
+                        <h1 className="text-5xl font-bold text-left">Books to freshen up <br /> your bookshelf</h1>
                         
-                        <button className="btn btn-primary">View The List</button>
+                        <button className="btn bg-[#23BE0A] w-5/12 text-white font-semibold mt-6 ">View The List</button>
                     </div>
                 </div>
             </div>
--- src/components/Books.jsx
+++ src/components/Books.jsx
@@ -4,14 +4,18 @@ import UseBooks from "./Hooks/UseBooks";
 
 
 const Books = () => {
-    const [data,loading]= UseBooks();
+    const [data, loading] = UseBooks();
     // console.log(data)
     return (
         <div>
             {/* <UseBooks></UseBooks> */}
-            {
-                data.map(book=><BookCard key={book.bookId} book={book}></BookCard>)
-            }
+            <h1 className="text-4xl font-bold mt-16 mb-7">Books</h1>
+            <div className="grid grid-cols-1 md:grid-cols-3 gap-6 mb-16">
+                
+                {
+                    data.map(book => <BookCard key={book.bookId} book={book}></BookCard>)
+                }
+            </div>
         </div>
     );
 };
