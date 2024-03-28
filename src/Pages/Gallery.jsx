7896d891477fd58d4d73f978bb9de3094efa650a
sujonifty <sujonifty@gmail.com>
2024-03-28 14:10:16 +0600

    completed gallery section

--- src/Pages/Gallery.jsx
+++ src/Pages/Gallery.jsx
@@ -4,10 +4,36 @@ const Gallery = () => {
 
     return (
         <div>
-            <div className="text-3xl font-bold  p-5 rounded-xl my-4 text-center bg-gray-200">
-                <h1>Books Gallery</h1>
+            <div className=" text-xl lg:text-5xl mt-24 font-bold mb-10 lg:10 lg:p-5 rounded-xl  text-center border-b-4 border-gray-300 bg-gray-200">
+                <h1 >A Visual Voyage through  books</h1>
             </div>
-            <section className="py-6 my-16 bg-gray-200 dark:bg-gray-100 dark:text-gray-900">
+            <div className="lg:p-5 mx-auto sm:p-10 md:p-16 bg-[#37475aff] mb-24 ">
+                <div className="flex flex-col max-w-3xl mx-auto overflow-hidden rounded">
+                    <img src="https://i.ibb.co/h2mRwJ2/book2.jpg" alt="" className="w-full h-60 sm:h-96 " />
+                    <div className="p-6 pb-12 m-4 mx-auto -mt-16 space-y-6 lg:max-w-2xl sm:px-10 sm:mx-12 lg:rounded-md text-white bg-[#262626]">
+                        <div className="space-y-2">
+                            <a rel="noopener noreferrer" href="#" className="inline-block text-2xl font-semibold sm:text-3xl">Dive into a World of Stories: Welcome to Our Books Gallery</a>
+                            <p className="text-xs ">By:
+                                <a rel="noopener noreferrer" href="#" className="text-xs ml-1 hover:underline">Sujonifty</a>
+                            </p>
+                        </div>
+                        <div className="dark:text-gray-800">
+                            <p>
+                                Welcome to our Books Gallery, a haven for book lovers and explorers of the written word. Here, amidst a vibrant collection of literary treasures, you'll find a tapestry of stories waiting to be unraveled. From gripping thrillers to heartwarming classics, each book holds the promise of adventure, enlightenment, and pure escapism. Join us as we open the doors to imagination and invite you to embark on a journey through the boundless realms of literature.
+                            </p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div className="text-3xl font-bold  lg:p-5 rounded-xl mt-10 text-center border-b-4 border-gray-300 bg-gray-200">
+                <h1>Books Gallery</h1>
+                <div className="text-gray-800 text-base w-11/12 mx-auto m-5">
+                    <p>
+                        where literary treasures await discovery amidst a vibrant collection. Each book promises adventure, enlightenment, and pure escapism, from gripping thrillers to heartwarming classics. Join us as we invite you to embark on a journey through the boundless realms of literature.
+                    </p>
+                </div>
+            </div>
+            <section className="py-6 mb-16 bg-gray-200 dark:bg-gray-100 dark:text-gray-900">
                 <div className="container grid grid-cols-2 gap-4 p-4 mx-auto md:grid-cols-4 ">
                     <img src="https://i.ibb.co/5WNw12x/b8.png" alt="" className="hover:scale-75 w-full h-full bg-cover col-span-2 row-span-2 bg-blue-200 border-blue-300 border-4 rounded shadow-sm min-h-96 md:col-start-3 md:row-start-1 dark:bg-gray-500 aspect-square" />
                     <img alt="" className="hover:scale-150 w-full h-full rounded shadow-xl min-h-48 bg-blue-200 dark:bg-white-100 border-blue-300 border-4  aspect-square" src="https://i.ibb.co/SVry0CD/b0.png" />
@@ -22,15 +48,24 @@ const Gallery = () => {
                 </div>
             </section>
 
+            <div className="container w-full max-w-4xl lg:p-6 mx-auto  rounded-md ">
+               
+                <div className="lg:p-4 space-y-2 text-sm text-left text-gray-600">
+                    <h1 className="text-3xl font-bold text-violet-700 my-5">Book Imports: Using Open Library & Google Books APIs</h1>
+                    <p>In today's digital age, importing books has become easier than ever thanks to Open Library and Google Books APIs. This article explores how these APIs streamline the process of adding books to digital libraries and online platforms.</p>
+                    <p>Discover the simplicity of adding books to your collection with Open Library and Google Books APIs. This article outlines the straightforward process of importing books from these vast repositories, making it easier than ever to expand your library's offerings.</p>
+                </div>
+            </div>
+
             <section className="bg-gray-100">
                 <div className="container flex flex-col justify-center items-center p-6 mx-auto sm:py-12 lg:py-24 lg:flex-row lg:justify-between">
                     <div className="flex items-center justify-center lg:p-6 mt-8 lg:mt-0 h-72 sm:h-80 lg:h-96 xl:h-112 2xl:h-128">
                         <img src="https://i.ibb.co/4RC8sgC/apibook.webp" alt="" className="object-contain h-72 sm:h-80 lg:h-96 xl:h-112 2xl:h-128" />
                     </div>
-                    <div className="flex  rounded-xl  flex-col justify-center lg:p-6 text-center rounded-sm lg:max-w-md xl:max-w-lg lg:text-left">
+                    <div className="flex  rounded-xl  flex-col justify-center lg:p-6 text-center  lg:max-w-md xl:max-w-lg lg:text-left">
                         <h1 className=" lg:text-5xl font-bold leading-none sm:text-xl">Import Books
 
-                            <span className="dark:text-violet-600">via API</span>From Open Library & Google Books.
+                            <span className="dark:text-violet-600">via API </span>From Open Library & Google Books.
                         </h1>
                         <p className="mt-6 mb-8 text-lg sm:mb-12">This is a handy tool that saves a lot of typing. All you need is the ISBN 10 number of the book you want to import.
                             <br className="hidden md:inline lg:hidden" />The importer brings in all the book details from the Google Books Archive. You can add multiple ISBNs at once, separated by a comma.
