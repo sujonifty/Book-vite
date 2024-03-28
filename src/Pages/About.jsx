aa42f4ceab2fc636f8e474d8ca4a6492dca59285
sujonifty <sujonifty@gmail.com>
2024-03-28 19:35:39 +0600

    completed about section

--- src/Pages/About.jsx
+++ src/Pages/About.jsx
@@ -39,8 +39,32 @@ const About = () => {
                             <h1 className="text-2xl">RESULTS</h1>
                             <p>At [Organization Name], we prioritize outcomes. Through strategic planning and relentless effort, we consistently deliver impactful results that drive our mission forward. With a focus on innovation and collaboration, we strive to exceed expectations and make a meaningful difference in the world.</p>
                         </div>
-                    
+
                     </div>
+                </div>
+            </section>
+
+            <section className=" lg:p-6 bg-[#37475aff]">
+                <div className="container grid gap-6 mx-auto text-center lg:grid-cols-2 xl:grid-cols-5">
+                    <div className="w-full px-6 py-16 rounded-md sm:px-12 md:px-16 xl:col-span-2 dark:bg-gray-50">
+                        <span className="block mb-2 dark:text-violet-600">Mamba design system</span>
+                        <h1 className="text-5xl font-extrabold dark:text-gray-900">Build it with Mamba</h1>
+                        <p className="my-8">
+                            <span className="font-medium dark:text-gray-900">Modular and versatile.</span>Fugit vero facilis dolor sit neque cupiditate minus esse accusamus cumque at.
+                        </p>
+                        <form novalidate="" action="" className="self-stretch space-y-3">
+                            <div>
+                                <label for="name" className="text-sm sr-only">Your name</label>
+                                <input id="name" type="text" placeholder="Your name" className="w-full rounded-md focus:ring focus:dark:ring-violet-600 dark:border-gray-300" />
+                            </div>
+                            <div>
+                                <label for="lastname" className="text-sm sr-only">Email address</label>
+                                <input id="lastname" type="text" placeholder="Email address" className="w-full rounded-md focus:ring focus:dark:ring-violet-600 dark:border-gray-300" />
+                            </div>
+                            <button type="button" className="w-full py-2 font-semibold rounded dark:bg-violet-600 dark:text-gray-50">Join the waitlist</button>
+                        </form>
+                    </div>
+                    <img src="https://i.ibb.co/KFs0ykY/bbbb.jpg" alt="" className="object-cover w-full rounded-md xl:col-span-3 dark:bg-gray-500" />
                 </div>
             </section>
 
